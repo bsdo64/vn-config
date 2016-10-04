e12134dea33023e6f0d2333b4c440582f4f18e4a
ByeongSu Do <bsdo@naver.com>
2016-10-04 22:37:32 +0900

    update configuration

--- front/session.js
+++ front/session.js
@@ -6,13 +6,13 @@ const conf = process.env.NODE_ENV==='production' ? {
   name: 'sessionId',
   resave: false,
   saveUninitialized: false,
-  cookie: {httpOnly: true, domain: 'venacle.com'}
+  cookie: {httpOnly: true, domain: 'venacle.com', maxAge: 7 * 24 * 60 * 60 * 1000}
 } : {
   secret: '1234567890QWERTY',
   name: 'sessionId',
   resave: false,
   saveUninitialized: false,
-  cookie: {httpOnly: true, domain: 'localhost'}
+  cookie: {httpOnly: true, domain: 'localhost', maxAge: 7 * 24 * 60 * 60 * 1000}
 };
 
 module.exports = conf;
--- redis/server.js
+++ redis/server.js
@@ -2,6 +2,7 @@
  * Created by dobyeongsu on 2016. 6. 28..
  */
 module.exports = {
+  ttl: 8 * 24 * 60 * 60,
   db: 0,
   port: 6379,
   host: '127.0.0.1'
